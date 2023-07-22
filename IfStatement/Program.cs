865339cd31a85b4321497802e1f9b91377487aae
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-07-22 10:55:40 -0600

    Agregar archivos de proyecto.

---
+++ IfStatement/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("What is your name: ");
+string name = Console.ReadLine();
+if (name.ToLower()== "héctor")
+{
+    Console.WriteLine("Hi professor Héctor");
+}else
+{
+    Console.WriteLine("Hi student.");
+}
